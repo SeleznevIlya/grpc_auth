0d7d3934295683259e5b12a1b401a61bbd0791ab
Ilya Seleznev <seleznev.ilya.a@gmail.com>
2024-06-12 22:05:24 +0300

    implement AuthServer

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -5,24 +5,119 @@ import (
 
 	grpc_authv1 "github.com/SeleznevIlya/protos/gen/go/grpc_auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const (
+	emptyValue = 0
+)
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		apiID int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 type serverApi struct {
 	grpc_authv1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	grpc_authv1.RegisterAuthServer(gRPC, &serverApi{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	grpc_authv1.RegisterAuthServer(gRPC, &serverApi{auth: auth})
 }
 
 func (s *serverApi) Login(ctx context.Context, req *grpc_authv1.LoginRequest) (*grpc_authv1.LoginResponse, error) {
-	panic("implement me")
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &grpc_authv1.LoginResponse{
+		Token: token,
+	}, nil
 }
 
 func (s *serverApi) Register(ctx context.Context, req *grpc_authv1.RegisterRequest) (*grpc_authv1.RegisterResponse, error) {
-	panic("implement me")
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &grpc_authv1.RegisterResponse{
+		UserId: userID,
+	}, nil
+
 }
 
 func (s *serverApi) IsAdmin(ctx context.Context, req *grpc_authv1.IsAdminRequest) (*grpc_authv1.IsAdminResponse, error) {
-	panic("implement me")
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &grpc_authv1.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
+}
+
+func validateLogin(req *grpc_authv1.LoginRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return nil
+}
+
+func validateRegister(req *grpc_authv1.RegisterRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	return nil
+}
+
+func validateIsAdmin(req *grpc_authv1.IsAdminRequest) error {
+	if req.GetUserId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "user_id is required")
+	}
+
+	return nil
 }
